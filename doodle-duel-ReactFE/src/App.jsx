d71030940afd52494edaf5134952fa800290cf6e
DeathRiderAnt <ant_odin@hotmail.co.uk>
2025-11-25 09:24:34 +0000

    Add socket.io client

--- doodle-duel-ReactFE/src/App.jsx
+++ doodle-duel-ReactFE/src/App.jsx
@@ -1,9 +1,25 @@
-import { useState } from "react";
+import { useEffect } from "react";
+import socket from "./socket"
 import Canvas from "./Components/Canvas";
 import LandingPage from "./Components/LandingPage";
 import { Routes, Route } from "react-router-dom";
 
 function App() {
+  useEffect(() => {
+    socket.on("connect", () => {
+      console.log("Socket connected:", socket.id);
+    });
+
+    socket.on("disconnect", () => {
+      console.log("Socket disconnected");
+    });
+
+    return () => {
+      socket.off("connect");
+      socket.off("disconnect");
+    };
+  }, []);
+
   return (
     <>
         <main>
